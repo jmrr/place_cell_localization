349594bd4078c23f5cbebef7b0b0a6dc804d1b1c
Jose Rivera <jose.rivera@imperial.ac.uk>
2015-01-22 13:05:45 +0000

    Achieved training and testing but concatenation might be wrong, trying a different concatenation of experiments

--- neuralnetRegression.m
+++ neuralnetRegression.m
@@ -7,7 +7,7 @@ setup;
 paramsCells.numCells = 8;
 
 
-%% Get kernels
+%% Get training kernels
 [results, trainingSet] = getKernel(paramsDataset, paramsQuery);
 
 kernels = results.Kernel;
@@ -61,7 +61,7 @@ for i = 1:paramsCells.numCells
         % If max of the peaks is the first or last sample take the max
         try
             [maxPeak, maxPeakIdx] = max(pks);
-            maxIdx(i,j) = locs(maxPeakIdx);
+            maxIdx(i,j)     = locs(maxPeakIdx);
         catch
             [maxPeak, maxIdx(i,j)] = max(cell);
         end
@@ -101,4 +101,96 @@ end
 net = newgrnn(input, gtPlaceCell);
 
 
+%% Obtain test data
+
+% Query locations
+
+bound     = round( paramsCells.sideSpan/2);
+queryLocs  = linspace(bound,numFramesCorr-bound, paramsCells.numCells);
+posQueryLoc = queryGt(round(queryLocs));
+
+% Query kernel and data
+
+kernelPath     = sprintf(paramsDataset.kernelPath,paramsDataset.encoding, ...
+    paramsDataset.descriptor,paramsQuery.queryCorridor);
+queryKernelFname = sprintf(paramsQuery.kernelStr,paramsQuery.queryCorridor,paramsDataset.encoding,...
+    paramsDataset.kernel,num2str(paramsQuery.queryPass),paramsQuery.queryPass);
+
+queryResults = load(fullfile(kernelPath,queryKernelFname));
+
+queryKernel = queryResults.Kernel;
+%%
+
+for i = 1:length(queryLocs)
+    
+    scores = queryKernel{1}(:,round(queryLocs(i)));
+    
+    
+    fr = round(queryLocs(i)); % In  the tuning curve, take the centre frame as
+                          % the one corresponding to the g.t. in the query
+                          % frames axis.
+    
+    % Prevent tuning curve out of bounds
+    
+    [lowerBound, upperBound] = getBounds(fr, paramsCells.sideSpan, numFramesCorr);
+    
+    c1 = scores(lowerBound:fr);
+    c2 = scores(fr+1:upperBound);
+    
+    queryCurves(:,i)  = [c1; c2];
+    
+end
+
+%% 
+
+% query gt
+
+maxQueryIdx         = zeros(paramsCells.numCells,1);
+queryGtPositions = zeros(paramsCells.sideSpan*2,paramsCells.numCells);
+
+for i = 1:paramsCells.numCells
+    
+        queryTuningCurve = queryCurves(:,i); % Get the tuning curve for the query
+        [pks, locs] = findpeaks(double(queryTuningCurve));  % Find the peak and its locations
+        
+        % If max of the peaks is the first or last sample take the max
+        try
+            [maxPeak, maxPeakIdx] = max(pks);
+            maxQueryIdx(i) = locs(maxPeakIdx);
+        catch
+            [maxPeak, maxQueryIdx(i)] = max(queryTuningCurve);
+        end
+% % Verification plot        
+%         plot(cell)
+%         hold on
+%         plot(maxIdx(i,j), maxPeak,'.r', 'LineWidth',4);
+%         hold off;
+        
+        % In this case we input the g.t. locations in the DB axis as we're
+        % testing and shouln't know the g.t. position of the query.
+        
+        % This bit here is to retrieve the actual frame positions of the
+        % tuning curves in the whole kernel response for that particular
+        % location.
+        
+        [lowerBound, upperBound] = getBounds(round(queryLocs(i)), ...
+            paramsCells.sideSpan, numFramesCorr);
+        
+        span = lowerBound:upperBound;
+        
+        % Exact firing location
+        qFiringLoc = span(maxQueryIdx(i));
+        
+        % Ground Truth query cell
+        qGtFiringLoc = queryGt(firingLoc);
+        queryGtPositions(:,i) = queryGt(span) - qGtFiringLoc;
+
+   
+end
+
+%%  Test (simulate)
+
+locEstimate = sim(net, queryCurves);
+
+errorInMetres = abs(locEstimate - queryGtPositions)/100;
 
