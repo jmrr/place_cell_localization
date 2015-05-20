c8eb47c79f8cfcbe92097a251423769aef0d0edd
Jose Rivera <jose.rivera@imperial.ac.uk>
2015-05-20 10:01:09 +0100

    Changed loc estimation condition

--- correctLocEstimates.m
+++ correctLocEstimates.m
@@ -1,9 +1,9 @@
 function locEstCorrected = correctLocEstimates(locEstimate, model)
 
-    if model.NormFlag == 0
-        locEstCorrected = locEstimate +  model.QueryLocations(round(end/2));
-    else
-        locEstCorrected = locEstimate.*(max(model.Observations));
-    end
+if model.NormFlag > 0
+    locEstCorrected = locEstimate.*(max(model.Observations));
+else
+    locEstCorrected = locEstimate +  model.QueryLocations(round(end/2));
+end
 
 end
